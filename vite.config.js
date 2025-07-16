a880842fee1e2dd4b8db77ade1931c14a627296d
Antony-css <antonygeorgefl@gmail.com>
2025-07-17 01:45:24 +0300

    Fix asset paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/saint-mark-leagues/',
+  base: '/saint-mark-leagues/',
   plugins: [react()],
 })
